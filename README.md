56b3c53f563f958960568a2e8bd6e20fe098f969
tofilwiktor <wiktorwk27@gmail.com>
2022-10-10 14:50:06 +0200

    commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#Mandelbrot set plotter in SFML/C++
+
+![mandelbrot set](mandelbrot.png)
--- main.cpp
+++ main.cpp
@@ -3,23 +3,24 @@
 #include <tuple>
 #include <SFML/Graphics.hpp>
 
-#define WIDTH 1280
-#define HEIGHT 720
+int WIDTH = 1280;
+int HEIGHT = 720;
+
 
 const int N3 = (int) pow(256, 3);
 
 std::tuple<int, int, int> n_to_rgb(int n, int max_iter) {
 	long double n_normal = (long double)n / (long double)max_iter;
-        unsigned short N = 256;
+        int N = 256;
         
         n = (int)(n_normal * (long double) N3);
 
-        int b = n / (N*N);
+        int g = n / (N*N);
 
-        int n_normal2 = n - b * N * N;
+        int n_normal2 = n - g * N * N;
 
-        int r = n_normal2 / N;
-        int g = n_normal2  - r * N;
+        int r = n_normal2 / N * N;
+        int b = n_normal2 - r * N;
         
 
         return std::tuple<int, int, int>(r, g, b);
@@ -27,7 +28,9 @@ std::tuple<int, int, int> n_to_rgb(int n, int max_iter) {
 
 
 void mandelbrot(sf::VertexArray& va, int shift_x, int shift_y, int max_iter, float zoom) {
-	for (int i = 0; i < HEIGHT; i++) {
+
+    #pragma omp parallel for
+    for (int i = 0; i < HEIGHT; i++) {
 		for (int j = 0; j < WIDTH; j++) {
 			long double x = ((long double)(j - shift_x)) / zoom;
 			long double y = ((long double)(i - shift_y)) / zoom;
@@ -47,35 +50,79 @@ void mandelbrot(sf::VertexArray& va, int shift_x, int shift_y, int max_iter, flo
 
 				n++;
 
-				if ((real(z) * real(z) + imag(z) * imag(z)) > 4) {
+				if (real(z) * real(z) + imag(z) * imag(z) > 4) {
 					break;
 				}
 			}
                         std::tuple<int, int, int> temp = n_to_rgb(n, max_iter);
 
-                        va[i*width + j].position = sf::Vector2f(j, i);
+                        va[i*WIDTH + j].position = sf::Vector2f(j, i);
                         sf::Color color(std::get<0>(temp), std::get<1>(temp), std::get<2>(temp));
 
-                        val[i * width + j].color = color;
+                        va[i * WIDTH + j].color = color;
 		}
 	}
+
 }
 
 int main () {
-	
+        auto desktop = sf::VideoMode::getDesktopMode();	
 	sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Mandelbrot");
         
-        window.setFramerateLimit(30);
+        window.setPosition(sf::Vector2i(desktop.width / 2 - window.getSize().x / 2, desktop.height / 2 - window.getSize().y / 2));
+
+        window.setFramerateLimit(60);
         sf::VertexArray pixels(sf::Points, WIDTH * HEIGHT);
 
+
+
         float zoom = 200.f;
-        int max_iter = 500;
+        int max_iter = 100;
         int shift_x = WIDTH / 2;
         int shift_y = HEIGHT / 2;
 
+
+        //mirrors the mandelbrot set on 1st generation
+        HEIGHT /= 2;     
+
         mandelbrot(pixels, shift_x, shift_y, max_iter, zoom);
+        
+        HEIGHT *= 2;
 
+        for (int i = HEIGHT / 2; i < HEIGHT; i++) {
+            for (int j = 0; j < WIDTH; j++) {
+                pixels[i*WIDTH + j].position = sf::Vector2f(j, HEIGHT - i + HEIGHT / 2);
+                pixels[i*WIDTH + j].color = pixels[(i-HEIGHT/2)*WIDTH + j].color;
+            }
+        }
 
+        while(window.isOpen()) {
+            sf::Event event;
 
+            while(window.pollEvent(event)) {
+                if (event.type == sf::Event::Closed) window.close();
+            }
+
+            if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+                sf::Vector2i pos = sf::Mouse::getPosition(window);
+                shift_x -= pos.x - shift_x;
+                shift_y -= pos.y - shift_y;
+
+                zoom *= 2;
+                //uncomment for increased precision after every zoom
+        //        max_iter += 100;
+
+                for (int i = 0; i < WIDTH * HEIGHT; i++) {
+                    pixels[i].color = sf::Color::Black;
+                }
+
+                mandelbrot(pixels, shift_x, shift_y, max_iter, zoom);
+            }
+            window.clear();
+            window.draw(pixels);
+            window.display();
+        }
+
+ 
 	return EXIT_SUCCESS;
 }
